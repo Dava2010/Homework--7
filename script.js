88a7fdbc46e66a1bfba928605e3e4c23e5ddf4cc
Dava2010 <davlatbek20101106@gmail.com>
2025-04-09 18:19:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const options = {
+  strings: ["TEXT GRADIENTS", "COLORFUL TITLES", "AWESOME HEADLINES"],
+  typeSpeed: 70,
+  backSpeed: 40,
+  startDelay: 500,
+  loop: true,
+};
+
+const typed = new Typed("#typed-text", options);
